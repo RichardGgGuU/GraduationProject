d61f895352029a727871fb0d647c06fe71f45147
RichardGgGuU <123468722+RichardGgGuU@users.noreply.github.com>
2025-05-08 13:08:40 +0800

    aliyun使用版本

--- backend/src/main/java/com/example/springboot/mapper/AdjustRoomMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.example.springboot.entity.AdjustRoom;
-
-public interface AdjustRoomMapper extends BaseMapper<AdjustRoom> {
-    Page selectall(Page page);
-}
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,17 +3,17 @@ module.exports = {
 
     publicPath: './',// vue-cli3.3+新版本使用
 
-    devServer: {
+    //devServer: {
 
-        proxy: {                 //设置代理
-            '/api': {              //设置拦截器  拦截器格式   斜杠+拦截器名字
-                target: 'http://localhost:9090',     //代理的目标地址
-                changeOrigin: true,              //是否设置同源，输入是的
-                pathRewrite: {                   //路径重写
-                    '^/api': ''                     //选择忽略拦截器里面的内容
-                }
-            }
-        }
-    }
+       // proxy: {                 //设置代理
+        //    '/api': {              //设置拦截器  拦截器格式   斜杠+拦截器名字
+         //       target: 'http://localhost:9090',     //代理的目标地址
+          //      changeOrigin: true,              //是否设置同源，输入是的
+           //     pathRewrite: {                   //路径重写
+           //         '^/api': ''                     //选择忽略拦截器里面的内容
+            //    }
+            //}
+       // }
+    //}
 }
 
